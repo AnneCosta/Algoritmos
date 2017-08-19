8929c53bec3a58db2074580533ebd862d8f12237
Fabianne Costa <fabiannecs@hotmail.com>
2017-08-19 10:42:49 -0300

    Add files via upload

---
+++ matrizatividade3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+int main() {
+	setlocale(LC_ALL, "portuguese_Brazil");
+	float notas[3][3], media;
+	int cont=1, linha;
+	for (linha=0; linha<3; linha++) {
+		printf("Informe a %d° nota do trabalho: ", cont++);
+		scanf("%f", &notas[linha][0]);
+		printf("Informe a %d° nota da prova: ", cont++);
+		scanf("%f", &notas[linha][1]);
+		notas[linha][2]=(notas[linha][0]+notas[linha][1])/2;
+	}
+	printf("\nTrab. / Prova / Média");
+	for(linha=0; linha<3; linha++) {
+		printf("\n[%.1f]   [%.1f]   [%.1f]", notas[linha][0], notas[linha][1], notas[linha][2]);
+	}
+}
