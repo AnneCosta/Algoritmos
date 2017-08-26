211610ac216623db2eec5a5bb05622d55b7925a9
Fabianne Costa <fabiannecs@hotmail.com>
2017-08-26 11:22:48 -0300

    Update funcaocomvetor.c

--- funcaocomvetor.c
+++ funcaocomvetor.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <locale.h>
-#define tam 10
+#include <stdlib.h>
 int menu () {
 	setlocale(LC_ALL,"portuguese_Brazil");
 	int r;
-	printf("----- VETOR -----\n");
 	printf("1 - MAIOR NÚMERO\n");
 	printf("2 - MENOR NÚMERO\n");
 	printf("3 - SOMA DOS VALORES\n");
@@ -12,38 +11,53 @@ int menu () {
 	printf("5 - SAIR\n");
 	printf("-----------------\n");
 	printf("\nDIGITE SUA OPÇÃO: ");
-	scanf("%d", &r);
+	scanf("%d\n", &r);
 	return r;
 }
 void maior (int v[]) {
 	int x, y, aux;
 	aux=v[0];
-	for (x=1; x<tam; x++) {
+	for (x=1; x<10; x++) {
 		if (v[x]>aux) aux=v[x];
 	}
-	printf("\nMaior valor do vetor: %d\n\n", aux);
+	printf("\nMaior valor do vetor: %d\n", aux);
 }
 void menor (int v[]) {
-	int x, y, aux;
+	int x, aux;
 	aux=v[0];
-	for (x=1; x<tam; x++) {
+	for (x=1; x<10; x++) {
 		if (v[x]<aux) aux=v[x];
 	}
-	printf("\nMenor valor do vetor: %d\n\n", aux);
+	printf("\nMenor valor do vetor: %d\n", aux);
 }
-/*void soma (int x, int y) {
-
+void soma (int v[]) {
+	int x, soma=0;
+	for (x=1; x<10; x++) {
+		soma=soma+v[x];
+	}
+	printf("Soma: %d\n", soma);
 }
-void todos (int x, int y) {
-
-}*/
+void todos (int v[]) {
+	int x;
+	for (x=1; x<10; x++) {
+		printf("|%d|\t\n", v[x]);
+	}
+}
 
 main () {
 	int v[10], x, y, r;
-	for (x=0; x<10; x++) {
-	printf("Informe o valor %d do vetor 1: ", x+1);
-	scanf("%d", &v[x]);
+	printf("----- VETOR -----\n");
+	r=menu();
+	while (r!=5) {
+	for(x=1; x<10; x++) {
+		v[x]=1+rand()%15;
+	}
+	system("cls");
+	printf("----- VETOR -----\n");
 	if (r==1) maior(v);
 	if (r==2) menor(v);
+	if (r==3) soma(v);
+	if (r==4) todos(v);
+	r=menu();
 	}
 }
