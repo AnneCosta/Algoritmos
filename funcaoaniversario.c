eeafd0ff15ca9ac4742609365e393239553dd6a6
Fabianne Costa <fabiannecs@hotmail.com>
2017-09-22 00:45:47 -0300

    Update funcaoaniversario.c

--- funcaoaniversario.c
+++ funcaoaniversario.c
@@ -6,37 +6,37 @@ int mes (int d, int m, int a){
 	if(m==1) {
 		printf("\nAniversário em %d de janeiro de %d.\n", d, a);
 	}	
-	if(m==2) {
+	else if(m==2) {
 		printf("\nAniversário em %d de feveireiro de %d.\n", d, a);
 	}
-	if (m==3) {
+	else if (m==3) {
 		printf("\nAniversário em %d de março de %d.\n", d, a);
 	}
-	if(m==4) {
+	else if(m==4) {
 		printf("\nAniversário em %d de abril de %d.\n", d, a);
 	}
-	if(m==5) {
+	else if(m==5) {
 		printf("\nAniversário em %d de maio de %d.\n", d, a);
 	}
-	if(m==6) {
+	else if(m==6) {
 		printf("\nAniversário em %d de junho de %d.\n", d, a);
 	}
-	if(m==7) {
+	else if(m==7) {
 		printf("\nAniversário em %d de julho de %d.\n", d, a);
 	}
-	if(m==8) {
+	else if(m==8) {
 		printf("\nAniversário em %d de agosto de %d.\n", d, a);
 	}
-	if(m==9) {
+	else if(m==9) {
 		printf("\nAniversário em %d de setembro de %d.\n", d, a);
 	}
-	if(m==10) {
+	else if(m==10) {
 		printf("\nAniversário em %d de outubro de %d.\n", d, a);
 	}
-	if(m==11) {
+	else if(m==11) {
 		printf("\nAniversário em %d de novembro de %d.\n", d, a);
 	}
-	if(m==12) {
+	else if(m==12) {
 		printf("\nAniversário em %d de dezembro de %d.\n", d, a);
 	}
 	
