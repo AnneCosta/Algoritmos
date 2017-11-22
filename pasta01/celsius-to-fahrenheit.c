7ee2e22dc07a3c5a028d2c9f6d5ab0048a71e921
Fabianne Costa <fabiannecs@hotmail.com>
2017-11-22 00:03:15 -0300

    add printf

--- pasta01/celsius-to-fahrenheit.c
+++ pasta01/celsius-to-fahrenheit.c
@@ -8,7 +8,7 @@ add um comentário exemplo de teste
 int main (){
 	setlocale(LC_ALL,"portuguese_Brazil");
 	float F=0, C=0;
-	
+	printf("Informe a temperatura em graus Celsius: ");
 	scanf("%f",&C);
 	
 	F=(9*C+160)/5;
